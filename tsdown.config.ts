56932b2e3a5de97cbcd78f6dc46d75df706b7b35
Antti Hilja <hiljaaa@gmail.com>
2026-04-07 03:05:47 +0200

    Remove tsdown

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-  entry: './index.ts',
-  outDir: './dist',
-  platform: 'browser',
-  dts: true,
-  define: { 'import.meta.vitest': 'undefined' },
-})
